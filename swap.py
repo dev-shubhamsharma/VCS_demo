c1b6b7ad4e326316c4a9050c084e5edb05d94c5a
Shubham Sharma <shubhamsharma.gonda@gmail.com>
2025-10-17 12:11:03 +0530

    program done

---
+++ swap.py
@@ -0,0 +1,10 @@
+// To swap numbers using python
+x = input("Enter x : ")
+y = input("Enter y : ")
+
+print("Before swapping : x = ",x,", y = "y)
+temp = x
+x = y
+y = temp
+
+print("After swapping : x = ",x,", y = "y)
