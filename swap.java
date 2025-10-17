c202ed87e87708b60f8c45cbb543e4c554ec11de
Shubham Sharma <shubhamsharma.gonda@gmail.com>
2025-10-17 12:52:17 +0530

    java file added

---
+++ swap.java
@@ -0,0 +1,20 @@
+import java.uti.Scanner;
+
+class swap
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Enter x : ");
+        int x = sc.nextInt();
+        System.out.print("Enter y : ");
+        int y = sc.nextInt();
+
+        // swap using third variable
+
+        int temp = x;
+        x = y;
+        y = temp;
+
+        
+    }
+}
