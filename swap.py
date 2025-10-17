c2b990eb64ba215974caeabfc32bbcf3f2bbba49
Shubham Sharma <shubhamsharma.gonda@gmail.com>
2025-10-17 16:53:46 +0530

    other logic added

--- swap.py
+++ swap.py
@@ -1,8 +1,8 @@
-// To swap numbers using python
+#  To swap numbers using python
 x = input("Enter x : ")
 y = input("Enter y : ")
 
-print("Before swapping : x = ",x,", y = "y)
+print("Before swapping : x = ",x,", y = ",y)
 
 # swap using third variable
 
