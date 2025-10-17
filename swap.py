eabcb35164a32b6982be1a8fe812faf6b7adaa2e
Shubham Sharma <shubhamsharma.gonda@gmail.com>
2025-10-17 12:16:31 +0530

    program updated

--- swap.py
+++ swap.py
@@ -3,8 +3,17 @@ x = input("Enter x : ")
 y = input("Enter y : ")
 
 print("Before swapping : x = ",x,", y = "y)
-temp = x
-x = y
-y = temp
+
+# swap using third variable
+
+# temp = x
+# x = y
+# y = temp
+
+# without using third variable
+
+x = x + y
+y = x - y
+x = x - y
 
 print("After swapping : x = ",x,", y = "y)
