97922bb213f63cbd125d05f6b96e4d58aba82efb
Shubham Sharma <shubhamsharma.gonda@gmail.com>
2025-10-17 17:00:04 +0530

    Revert "added logic"
    This reverts commit 341022f175b022afd58f75dd7f4b9d8665e1dbe0.

--- swap.java
+++ swap.java
@@ -9,21 +9,12 @@ class swap
         System.out.print("Enter y : ");
         int y = sc.nextInt();
 
-        System.out.println("Before swapping x is "+x+" , Y is "+y);
         // swap using third variable
 
-        
-        // int temp = x;
-        // x = y;
-        // y = temp;
+        int temp = x;
+        x = y;
+        y = temp;
 
-        // swap without using third variable
-
-        x = x+y;
-        y = x-y;
-        x = x-y;
-
-        System.out.println("After swapping x is "+x+" , Y is "+y);
         
     }
 }
